a84204e88ec0a993f0c4f6e04ef3d8afec3f955d
Julian Edwards <julian.edwards@mongodb.com>
2019-06-18 10:57:57 -0400

    use wrapped interface

--- interface.go
+++ interface.go
@@ -5,3 +5,8 @@ import "github.com/square/certstrap/depot"
 // Depot is a wrapper around certrstap's depot.Depot interface so users only
 // need to vendor certdepot.
 type Depot depot.Depot
+
+// Wrapper to create a FileDepot certdepot.Depot.
+func NewFileDepot(dir string) (Depot, error) {
+	return depot.NewFileDepot(dir)
+}
